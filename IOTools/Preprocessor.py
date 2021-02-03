Fixed scaling

IOTools/Preprocessor.py
@@ -26,28 +26,17 @@ class Preprocessor():
         :param dataframe: input pandas dataframe
         :return: preprocessed pandas dataframe
         '''
-        abs_scale = ["deltaPhiTauMet", "deltaPhiTauBjet", "deltaPhiBjetMet"]
+        log_scale = ["MET", "tauPt", "bjetPt", "TransverseMass"]
+        #variables with weird values outside of range
         dataframe = self._clean_inputs(dataframe, ["ldgTrkPtFrac", "TransverseMass"], [0.0, 0.0], [1.0, None])
-        dataframe = self._abs_inputs(dataframe, ["deltaPhiTauMet", "deltaPhiTauBjet", "deltaPhiBjetMet"])
+        dataframe = self._abs_inputs(dataframe, self._variable_names)
+        dataframe.loc[:, log_scale] = np.log1p(dataframe.loc[:, log_scale])
+        self._mins = dataframe.min(axis=0).to_numpy()
+        self._maxs = dataframe.max(axis=0).to_numpy()
 
-        if (self._mins == None or self._maxs == None):
-            self._get_min_and_max_values(dataframe)
-        print(self._mins)
-        print(self._maxs)
-        for i, column in enumerate(self._variable_names):
-            dataframe.loc[:, column] = self._scale(dataframe.loc[:, column],
-                                                                        self._preprocessing_modes[i],
-                                                                       self._mins[i],
-                                                                       self._maxs[i])
-        print(dataframe.min(axis=0))
-        print(dataframe.max(axis=0))
+        dataframe = dataframe.subtract(self._mins)
+        dataframe = dataframe.divide((self._maxs-self._mins))
         return dataframe
-
-    def _get_min_and_max_values(self, dataframe):
-        _mins = dataframe.min(axis=0) #dataframe.loc[:, self._variable_names].apply(np.min, axis=0, raw=True).to_numpy()
-        _maxs = dataframe.max(axis=0) #dataframe.loc[:, self._variable_names].apply(np.max, axis=0, raw=True).to_numpy()
-        self._mins = _mins.to_numpy()
-        self._maxs = _maxs.to_numpy()
 
     def _clean_inputs(self, dataframe, inputs_to_clip, mins, maxs):
         '''
@@ -67,7 +56,7 @@ class Preprocessor():
         if mode == "MinMaxScale":
             x = (x-min)/(max-min)
         elif mode == "MinMaxLogScale":
-            x = np.log1p(x-min)/np.log1p(max-min)
+            raise NotImplementedError("MinMaxLog not yet implemented")
         elif mode == "StandardScale":
             raise NotImplementedError("StandardScale not yet implemented")
         elif mode == "StandardLogScale":

NeuralNetwork/Classifier.py
@@ -16,7 +16,8 @@ class Classifier():
                  optimizer='adam',
                  lr=1e-4,
                  layers=3,
-                 init_bias=0.5):
+                 init_bias=0.5,
+                 disco_factor=10.0):
         self._n_inputs = n_inputs
         self._neurons = neurons
         self._activation = activation
@@ -25,6 +26,7 @@ class Classifier():
         self._lr = lr
         self._layers = layers
         self._init_bias = init_bias
+        self._disco_factor = disco_factor
 
     def get_model(self):
         try:
@@ -37,26 +39,33 @@ class Classifier():
             ValueError("Use 'adam' optimizer instead")
         _initializer = 'lecun_normal'
 
-        input_layer = layers.Input(self._n_inputs, name="input_layer")
+        # input_layer = layers.Input(self._n_inputs, name="input_layer")
+        # dense_1 = layers.Dense(4096, activation=self._activation, kernel_initializer=_initializer)(input_layer)
+        # x = layers.Dense(2048, activation=self._activation, kernel_initializer=_initializer)(dense_1)
+        # x = layers.Dense(1024, activation=self._activation, kernel_initializer=_initializer)(x)
+        # x = layers.Dense(512, activation=self._activation, kernel_initializer=_initializer)(x)
+
+        input_layer = layers.Input(self._n_inputs, dtype=tf.float16, name="input_layer")
         x = input_layer
         for i in range(self._layers):
             x = layers.Dense(int(self._neurons/pow(2,i)),
                              activation=self._activation,
                              kernel_initializer=_initializer,
-                             activity_regularizer=l2(self._regularizer_magnitude),
+     #                        activity_regularizer=l2(self._regularizer_magnitude),
                              name="dense_{}".format((i+1)))(x)
-#            x = layers.BatchNormalization(name="batchnorm_{}".format((i+1)))(x)
-
+            x = layers.BatchNormalization(name="batchnorm_{}".format((i+1)))(x)
+#
         output_layer = layers.Dense(1, activation='sigmoid',
                                     kernel_initializer=_initializer,
                                     bias_initializer=Constant(self._init_bias))(x)
 
-        x = layers.Dense(1, kernel_initializer=_initializer,bias_initializer=Constant(self._init_bias), name="out_dense")(x)
-        output_layer = layers.Activation('sigmoid', dtype=tf.float32, name='predictions')(x)
+        # x = layers.Dense(1, kernel_initializer=_initializer,bias_initializer=Constant(self._init_bias), name="out_dense")(x)
+        # output_layer = layers.Activation('sigmoid', dtype=tf.float32, name='predictions')(x)
 
         model = Model(input_layer, output_layer)
-        loss = DiscoLoss(factor=0.0)
+        loss = DiscoLoss(factor=self._disco_factor)
         model.compile(optimizer=LossScaleOptimizer(_optimizer),
-                      loss='binary_crossentropy')#loss)
+#                      loss='binary_crossentropy')
+                      loss=loss)
         return model
 

main.py
@@ -4,10 +4,11 @@ from NeuralNetwork import Classifier
 import tensorflow as tf
 import numpy as np
 import pandas as pd
+import sys
 pd.set_option('display.max_columns', None)
 
-#from tensorflow.keras import mixed_precision
-#mixed_precision.set_global_policy('mixed_float16')
+from tensorflow.keras import mixed_precision
+mixed_precision.set_global_policy('mixed_float16')
 
 if __name__ == '__main__':
     tf.random.set_seed(1337)
@@ -18,12 +19,16 @@ if __name__ == '__main__':
     training_variables = ["MET", "tauPt", "ldgTrkPtFrac", "deltaPhiTauMet", "deltaPhiTauBjet",
                                "bjetPt", "deltaPhiBjetMet", "TransverseMass"]
     preprocess_modes = ["MinMaxScale", "MinMaxScale", "MinMaxScale", "MinMaxScale",
-                        "MinMaxScale", "MinMaxLogScale", "MinMaxScale", "MinMaxScale"]
+                        "MinMaxScale", "MinMaxScale", "MinMaxScale", "MinMaxScale"]
 
     preprocessor = Preprocessor(training_variables, preprocess_modes)
     dataframe_scaled = preprocessor.process(dataframe.copy())
 
-    classifier = Classifier(8, neurons=1024, lr=3e-4, regularizer_magnitude=1e-4)
+    print(dataframe_scaled.min())
+    print(dataframe_scaled.max())
+    sys.exit(1)
+
+    classifier = Classifier(8, neurons=8192, layers=5, lr=3e-4, regularizer_magnitude=1e-4, disco_factor=10.0)
     model = classifier.get_model()
     print(model.summary())
 
@@ -33,17 +38,18 @@ if __name__ == '__main__':
     signal = signal.sample(n=background.shape[0])
     training_frame = signal.append(background)
     training_frame = training_frame.sample(frac=1.0)
+    training_frame.astype('float16')
 
     disco_targets = np.column_stack([(training_frame.loc[:, "event_id"] == 0),
                                     training_frame.loc[:, "TransverseMass"],
                                     2*(training_frame.loc[:, "event_id"] != 0)])
 
     model.fit(
-#        {'input_layer': training_frame.loc[:, training_variables], 'label': disco_targets},
         training_frame.loc[:, training_variables],
-        (training_frame.loc[:, "event_id"] == 0),
+        # (training_frame.loc[:, "event_id"] == 0),
+        disco_targets,
         epochs=10,
-        batch_size=int(1024),
+        batch_size=int(2048),
         validation_split=0.1
     )
 
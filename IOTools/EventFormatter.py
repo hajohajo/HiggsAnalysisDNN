97b6e3bb98e212e0e3ba292bfcd37d734a2eaa6a
hajohajo <joona.havukainen@helsinki.fi>
2021-02-19 17:40:16 +0200

    Added list of variables

--- IOTools/EventFormatter.py
+++ IOTools/EventFormatter.py
@@ -1,3 +1,34 @@
 
+    needed_variables = ["event",
 
+                        "nGoodOfflineVertices",
+
+                        "HLT_LooseIsoPFTau50_Trk30_eta2p1_MET90_vx",
+
+                        "MET_Type1_x", "MET_Type1_y",
+                        "L1MET_pat_x", "L1MET_pat_y",
+
+                        "Taus_pt", "Taus_eta", "Taus_phi", "Taus_e",
+                        "Taus_decayModeFindingNewDMs", "Taus_againstElectronTightMVA6",
+                        "Taus_againstMuonLoose3", "Taus_byLooseIsolationMVArun2v1DBoldDMwLT",
+                        "Taus_lChTrkPt", "Taus_lChTrkEta", "Taus_nProngs", "Taus_pdgId",
+
+                        "Electrons_pt", "Electrons_eta", "Electrons_phi", "Electrons_e", "Electrons_MVA", "Electrons_effAreaMiniIso",
+
+                        "Muons_pt", "Muons_eta", "Muons_phi", "Muons_e", "Muons_muIDLoose", "Muons_effAreaMiniIso",
+
+                        "Jets_pt", "Jets_eta", "Jets_phi", "Jets_e", "Jets_IDloose", "Jets_pfCombinedInclusiveSecondaryVertexV2BJetTags",
+
+#                        "HLTBJet_pt", "HLTBJet_eta", "HLTBJet_phi", "HLTBJet_e",
+
+                        "METFilter_hbheNoiseTokenRun2Loose",
+                        "METFilter_Flag_HBHENoiseFilter",
+                        "METFilter_Flag_HBHENoiseIsoFilter",
+                        "METFilter_Flag_EcalDeadCellTriggerPrimitiveFilter",
+                        "METFilter_Flag_eeBadScFilter",
+                        "METFilter_Flag_goodVertices",
+                        "METFilter_Flag_globalTightHalo2016Filter",
+                        "METFilter_badPFMuonFilter",
+                        "METFilter_badChargedCandidateFilter"
+                        ]
 class EventFormatter():
